5b5d1db617adbfbea75cae780538c2e4a073e3be
Michael Kerr <Michael.kerr@ogilvy.com>
2016-05-03 17:43:46 +0100

    Finds all elements and prints them

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,5 +1,3 @@
-alert("Hello from your Chrome extension!");
-console.log("running");
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
@@ -9,6 +7,34 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
+		$.get(chrome.extension.getURL('/src/inject/inject.html'), function(data) {
+			$(data).appendTo('body');
+			console.log($('#keyline').children());
+			var container = createListItem($('#keyline').children());
+			var content = document.getElementById("ui-content");
+			content.appendChild(container);
+		});
+
+
+
+		function createListItem(el){
+			var container = document.createElement('div');
+			$(container).addClass('item');
+			if(el.length > 0) {
+				var list = document.createElement("ul");
+				for(var i=0; i < el.length; i++) {
+					var item = document.createElement("li");
+					$(item).text(el[i].nodeName+" id: "+el[i].id);
+					$(list).append(item);
+					if($(el[i]).children().length > 0) {
+						console.log("runs?");
+						$(item).append(createListItem($(el[i]).children()));
+					}
+				}
+				$(container).append($(list));
+			}
+			return container;
+		}
 
 	}
 	}, 10);
